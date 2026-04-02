096870bb6fd11d9fcce3601818baf6b529020098
Adrian <adrian@adrianthedev.com>
2026-04-02 14:26:36 +0300

    feat(i18n): add locale routing infrastructure for Japanese translation
    Set up path-prefix locale routing (/ja/...) with optional locale scope
    for public routes. Auth and admin routes remain outside the scope.
    Add set_locale and default_url_options to ApplicationController.
    Configure available_locales [:en, :ja] and set HTML lang attribute.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,42 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get root_url
+    assert_response :success
+  end
+
+  test "should get index with Japanese locale" do
+    get root_url(locale: :ja)
+    assert_response :success
+  end
+
+  test "should get organizer" do
+    get organizer_url
+    assert_response :success
+  end
+
+  test "should get organizer with Japanese locale" do
+    get organizer_url(locale: :ja)
+    assert_response :success
+  end
+
+  test "should get organizer checklist" do
+    get organizer_checklist_url
+    assert_response :success
+  end
+
+  test "should get stamp samples" do
+    get stamp_samples_url
+    assert_response :success
+  end
+
+  test "should reject unsupported locale" do
+    get "/fr/"
+    assert_response :not_found
+  end
+
+  test "auth routes work without locale prefix" do
+    get "/session/new"
     assert_response :success
   end
 end
